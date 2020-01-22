5eaf00094877dc9fc8e654350b2e7060c92b4380
yasmin yazdi <yyazdi13@gmail.com>
2020-01-21 20:01:45 -0600

    heroku bugs

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "127.0.0.1",
-  port: 3306,
+  port: process.env.PORT || 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
