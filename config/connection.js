b35da4abf760c4d6343745ff8fee542df3e03830
yasmin yazdi <yyazdi13@gmail.com>
2020-01-21 19:45:24 -0600

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: process.env.PORT || 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var burger = require('./models/burger')
 var PORT = process.env.PORT || 3001;
 var exphbs = require("express-handlebars");
 var routes = require("./controllers/burgers_controller.js");
@@ -21,6 +20,3 @@ app.listen(PORT, function() {
   console.log("App now listening at localhost:" + PORT);
 });
 
-// burger.insertOne(["burger_name", "devoured"], ["cheese", "1"], function(result){
-//   console.log(result);
-// })
